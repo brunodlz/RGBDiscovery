0f0e4e6632ea812b6859c0c03efb32a43a2f2f6b
Bruno da Luz <brunolabx@gmail.com>
2015-05-14 09:06:13 -0300

    Initial Commit

---
+++ RGBDiscovery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGBDiscovery
+//
+//  Created by bruno da luz on 5/14/15.
+//  Copyright (c) 2015 bruno v0id. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
