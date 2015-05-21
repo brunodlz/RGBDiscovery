c36899f740bef631c780ebcadfc69d75928efa3f
Bruno da Luz <brunolabx@gmail.com>
2015-05-21 12:06:59 -0300

    Done!

--- RGBDiscovery/ViewController.swift
+++ RGBDiscovery/ViewController.swift
@@ -13,15 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderRed: UISlider!
     @IBOutlet weak var sliderGreen: UISlider!
     @IBOutlet weak var sliderBlue: UISlider!
+    @IBOutlet weak var sliderAlpha: UISlider!
     
-    @IBOutlet weak var redLabel: UITextField!
-    @IBOutlet weak var greenLabel: UITextField!
-    @IBOutlet weak var blueLabel: UITextField!
-    @IBOutlet weak var hexLabel: UITextField!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var hexLabel: UILabel!
     
     var fRed:   CGFloat = 0
     var fGreen: CGFloat = 0
     var fBlue:  CGFloat = 0
+    var fAlpha: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,18 +45,22 @@ class ViewController: UIViewController {
         
         UIView.animateWithDuration(1.0, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: nil, animations: { () -> Void in
             self.redLabel.center.x = self.view.frame.width / 2
+            self.sliderRed.center.x = self.view.frame.width / 2
             }, completion: nil)
         
         UIView.animateWithDuration(1.0, delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: nil, animations: { () -> Void in
             self.greenLabel.center.x = self.view.frame.width / 2
+            self.sliderGreen.center.x = self.view.frame.width / 2
             }, completion: nil)
         
         UIView.animateWithDuration(1.0, delay: 0.2, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: nil, animations: { () -> Void in
             self.blueLabel.center.x = self.view.frame.width / 2
+            self.sliderBlue.center.x = self.view.frame.width / 2
             }, completion: nil)
         
         UIView.animateWithDuration(1.0, delay: 0.3, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: nil, animations: { () -> Void in
             self.hexLabel.center.x = self.view.frame.width / 0.5
+            self.sliderAlpha.center.x = self.view.frame.width / 0.5
             }, completion: nil)
     }
     
@@ -68,19 +74,20 @@ class ViewController: UIViewController {
         self.fRed   = CGFloat(self.sliderRed.value)
         self.fGreen = CGFloat(self.sliderGreen.value)
         self.fBlue  = CGFloat(self.sliderBlue.value)
+        self.fAlpha  = CGFloat(self.sliderAlpha.value)
         
-        self.view.backgroundColor = UIColor(red: self.fRed, green: self.fGreen, blue: self.fBlue, alpha: 1)
+        self.view.backgroundColor = UIColor(red: self.fRed, green: self.fGreen, blue: self.fBlue, alpha: self.fAlpha)
     }
     
     func updateRGB() {
         var iRed = Int(self.fRed * 255)
-        self.redLabel.text = "R: \(iRed)"
+        self.redLabel.text = "\(iRed)"
         
         var iGreen = Int(self.fGreen * 255)
-        self.greenLabel.text = "G: \(iGreen)"
+        self.greenLabel.text = "\(iGreen)"
         
-        var (iBlue) = Int(self.fBlue * 255)
-        self.blueLabel.text = "B: \(iBlue)"
+        var iBlue = Int(self.fBlue * 255)
+        self.blueLabel.text = "\(iBlue)"
     }
     
     func updateHex() {
@@ -88,7 +95,7 @@ class ViewController: UIViewController {
         var sGreen = getGreenHex()
         var sBlue = getBlueHex()
         
-        self.hexLabel.text = "Hex: #\(sRed)\(sGreen)\(sBlue)"
+        self.hexLabel.text = "#\(sRed)\(sGreen)\(sBlue)".uppercaseString
     }
     
     func getRedHex() -> String {
