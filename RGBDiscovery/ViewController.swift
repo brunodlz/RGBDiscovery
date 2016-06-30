90a849e29966114b847797ad0fe5082678ea212e
brunodlz <brunolabx@gmail.com>
2016-06-30 20:50:39 -0300

    alpha removed

--- RGBDiscovery/ViewController.swift
+++ RGBDiscovery/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderRed: UISlider!
     @IBOutlet weak var sliderGreen: UISlider!
     @IBOutlet weak var sliderBlue: UISlider!
-    @IBOutlet weak var sliderAlpha: UISlider!
 
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
@@ -32,30 +31,28 @@ class ViewController: UIViewController {
     }
 
     func startWithAnimation() {
-        self.redLabel.center.x = self.view.frame.width + 30
-        self.greenLabel.center.x = self.view.frame.width + 30
-        self.blueLabel.center.x = self.view.frame.width + 30
+
+        let frameWidth = self.view.frame.width
+
+        self.redLabel.center.x   = frameWidth + 30
+        self.greenLabel.center.x = frameWidth + 30
+        self.blueLabel.center.x  = frameWidth + 30
 
         UIView.animateWithDuration(1.0) {
-            self.redLabel.center.x = self.view.frame.width / 2
-            self.sliderRed.center.x = self.view.frame.width / 2
+            self.redLabel.center.x  = frameWidth / 2
+            self.sliderRed.center.x = frameWidth / 2
         }
 
 
         UIView.animateWithDuration(1.0) {
-            self.greenLabel.center.x = self.view.frame.width / 2
-            self.sliderGreen.center.x = self.view.frame.width / 2
-            }
+            self.greenLabel.center.x  = frameWidth / 2
+            self.sliderGreen.center.x = frameWidth / 2
+        }
 
         UIView.animateWithDuration(1.0) {
-            self.blueLabel.center.x = self.view.frame.width / 2
-            self.sliderBlue.center.x = self.view.frame.width / 2
-            }
-
-        UIView.animateWithDuration(1.0) {
-            self.hexLabel.center.x = self.view.frame.width / 0.5
-            self.sliderAlpha.center.x = self.view.frame.width / 0.5
-            }
+            self.blueLabel.center.x  = frameWidth / 2
+            self.sliderBlue.center.x = frameWidth / 2
+        }
     }
 
     @IBAction func updateBackground() {
@@ -66,7 +63,6 @@ class ViewController: UIViewController {
         let red    = CGFloat(self.sliderRed.value)
         let green  = CGFloat(self.sliderGreen.value)
         let blue   = CGFloat(self.sliderBlue.value)
-        let alpha  = CGFloat(self.sliderAlpha.value)
 
         self.redLabel.text   = "\(self.sliderRed.number(red))"
         self.greenLabel.text = "\(self.sliderGreen.number(green))"
@@ -75,6 +71,6 @@ class ViewController: UIViewController {
         self.view.backgroundColor = UIColor(red: red,
                                             green: green,
                                             blue: blue,
-                                            alpha: alpha)
+                                            alpha: 1.0)
     }
 }
